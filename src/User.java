1e4d8f32e25b78a22ea7d322146ad57509555f0e
crunchwrap89 <marcus89n@gmail.com>
2020-05-21 14:05:29 +0200

    added code for User.java in Clientproj

--- src/User.java
+++ src/User.java
@@ -1,4 +1,32 @@
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement
 public class User {
 
+	private int id;
+	private String name;
+	private String surname;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return name;
+	}
+	public void setFirstName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+	public String toString() {
+		return "User (" + this.id + ") - " + this.name + " " + this.surname;
+	}
+	
 }
