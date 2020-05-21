8beae93c0992cdcef782f6a86f43ae84f3dc8966
AndersStationärDator <anders.maurits@hotmail.com>
2020-05-21 14:27:17 +0200

    added code to test client

--- src/TestClient.java
+++ src/TestClient.java
@@ -1,4 +1,41 @@
+import java.util.List;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.Response;
 
 public class TestClient {
 
+	public static void main(String[] args) {
+		Client client = ClientBuilder.newClient();
+		Response response = client.target("")
+				.request().buildGet().invoke();
+		
+		User employee = response.readEntity(User.class);
+		System.out.println(employee);
+		response.close();
+		
+		User alfred = new User();
+
+		
+		Entity benEntity = Entity.entity(alfred, "application/XML");
+		
+		response = client.target("http://localhost:8080/EmployeeManagement/webservice/employees")
+				.request().buildPost(benEntity).invoke();
+//		System.out.println(response.readEntity(User.class).getId());
+		response.close();
+		
+		
+		response = client.target("http://localhost:8080/EmployeeManagement/webservice/employees")
+				.request().buildGet().invoke();
+		List<User> users = response.readEntity(new GenericType<List<User>>() {});
+		
+		for (User u : users) {
+			System.out.println(u);
+		}
+	}
 }
