7ed23b01d9325c37aec781065b0ae72016e4d3ec
AndersStationärDator <anders.maurits@hotmail.com>
2020-05-25 12:21:44 +0200

    Fixed method to search by id in scanner

--- src/RunApp.java
+++ src/RunApp.java
@@ -1,6 +1,7 @@
 import java.util.List;
 import java.util.Scanner;
 
+import javax.ws.rs.ProcessingException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -16,7 +17,8 @@ public class RunApp {
 
 	public void appRunner() {
 		while (true) {
-			System.out.println(" Välj ett alternativ genom att skriva in en siffra för det alternativ du vill välja och tryck enter");
+			System.out.println(
+					" Välj ett alternativ genom att skriva in en siffra för det alternativ du vill välja och tryck enter");
 			System.out.println(" 1. Registrera en ny användare");
 			System.out.println(" 2. Lista alla användare");
 			System.out.println(" 3. Söka på användarid");
@@ -37,7 +39,7 @@ public class RunApp {
 					listAll();
 					sc.nextLine();
 					continue;
-				}else if (i == 3) {
+				} else if (i == 3) {
 					idSearch();
 					sc.nextLine();
 					continue;
@@ -59,43 +61,52 @@ public class RunApp {
 				sc2.nextLine();
 				continue;
 			} else {
-				// här ska metoden för idsökning anropas
-				System.out.println("Metod för idsökning inte inlagd än");
+				try {
+					String idnummer = "http://localhost:8080/UserManagement/webservice/users/" + sc2.nextInt();
+					Client client = ClientBuilder.newClient();
+					Response response = client.target(idnummer).request().buildGet().invoke();
+					User1 user2 = response.readEntity(User1.class);
+					System.out.println(user2);
+					response.close();
+
+				} catch (ProcessingException e) {
+					System.err.println("Kunde inte hitta någon person med det id:t");
+				}
 				break;
 			}
 		}
 	}
-	// Metod för att lägga till ny användare 
-	public static void newUser() {
 
+	// Metod för att lägga till ny användare
+	public static void newUser() {
 		System.out.println("Ange förnamn på användaren som ska registreras");
 		sc.nextLine();
 		s = sc.nextLine();
-//		System.out.println(s);
-		
 		System.out.println("Ange efternamn på användaren som ska registreras");
-		s1 = sc.nextLine();				
+		s1 = sc.nextLine();
 		addUser(s, s1);
-		System.out.println("User " + s + " " + s1 + " added" );
-
+		System.out.println("User " + s + " " + s1 + " added");
 	}
+
 	// Anropas av newUser när en ny användare läggs till
 	public static void addUser(String fornamn, String efternamn) {
 		Client client = ClientBuilder.newClient();
 		User1 anvandare = new User1();
 		anvandare.setName(fornamn);
-		anvandare.setSurname(efternamn);	
+		anvandare.setSurname(efternamn);
 		Entity alfredity = Entity.entity(anvandare, "application/XML");
-		Response response = client.target("http://localhost:8080/UserManagement/webservice/users").request().buildPost(alfredity)
-				.invoke();
+		Response response = client.target("http://localhost:8080/UserManagement/webservice/users").request()
+				.buildPost(alfredity).invoke();
 //		System.out.println(response.readEntity(User1.class).getId());
 		response.close();
-		
+
 	}
+
 	// Metod som skriver ut alla användare
 	public static void listAll() {
 		Client client = ClientBuilder.newClient();
-		Response response = client.target("http://localhost:8080/UserManagement/webservice/users").request().buildGet().invoke();
+		Response response = client.target("http://localhost:8080/UserManagement/webservice/users").request().buildGet()
+				.invoke();
 		List<User1> users = response.readEntity(new GenericType<List<User1>>() {
 		});
 
