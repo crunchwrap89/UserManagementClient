788953d4fd1e172694b05633ea2f78496e1895c9
AndersStationärDator <anders.maurits@hotmail.com>
2020-05-25 03:08:10 +0200

    started on scanner method

---
+++ src/RunApp.java
@@ -0,0 +1,107 @@
+import java.util.List;
+import java.util.Scanner;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.Response;
+
+public class RunApp {
+
+	private static Scanner sc = new Scanner(System.in);
+	private static String s;
+	private static String s1;
+	int i;
+
+	public void appRunner() {
+		while (true) {
+			System.out.println(" Välj ett alternativ genom att skriva in en siffra för det alternativ du vill välja och tryck enter");
+			System.out.println(" 1. Registrera en ny användare");
+			System.out.println(" 2. Lista alla användare");
+			System.out.println(" 3. Söka på användarid");
+			System.out.println(" 4. Avsluta");
+
+			if (sc.hasNextInt() == false) {
+				System.err.println("Du måste ange ett nummer");
+				sc.reset();
+				sc.nextLine();
+				continue;
+
+			} else {
+				i = sc.nextInt();
+				if (i == 1) {
+					newUser();
+					continue;
+				} else if (i == 2) {
+					listAll();
+					sc.nextLine();
+					continue;
+				}else if (i == 3) {
+					idSearch();
+					sc.nextLine();
+					continue;
+				} else if (i == 4) {
+					System.out.println("Hare gött!");
+					System.exit(0);
+				}
+			}
+		}
+	}
+
+	// Metdo för att söka via ID
+	private static void idSearch() {
+		while (true) {
+			System.out.println("skriv in id på personen du vill söka efter");
+			Scanner sc2 = new Scanner(System.in);
+			if (sc2.hasNextInt() == false) {
+				System.err.println("Du måste ange ett nummer");
+				sc2.nextLine();
+				continue;
+			} else {
+				// här ska metoden för idsökning anropas
+				System.out.println("Metod för idsökning inte inlagd än");
+				break;
+			}
+		}
+	}
+	// Metod för att lägga till ny användare 
+	public static void newUser() {
+
+		System.out.println("Ange förnamn på användaren som ska registreras");
+		sc.nextLine();
+		s = sc.nextLine();
+//		System.out.println(s);
+		
+		System.out.println("Ange efternamn på användaren som ska registreras");
+		s1 = sc.nextLine();				
+		addUser(s, s1);
+		System.out.println("User " + s + " " + s1 + " added" );
+
+	}
+	// Anropas av newUser när en ny användare läggs till
+	public static void addUser(String fornamn, String efternamn) {
+		Client client = ClientBuilder.newClient();
+		User1 anvandare = new User1();
+		anvandare.setName(fornamn);
+		anvandare.setSurname(efternamn);	
+		Entity alfredity = Entity.entity(anvandare, "application/XML");
+		Response response = client.target("http://localhost:8080/UserManagement/webservice/users").request().buildPost(alfredity)
+				.invoke();
+//		System.out.println(response.readEntity(User1.class).getId());
+		response.close();
+		
+	}
+	// Metod som skriver ut alla användare
+	public static void listAll() {
+		Client client = ClientBuilder.newClient();
+		Response response = client.target("http://localhost:8080/UserManagement/webservice/users").request().buildGet().invoke();
+		List<User1> users = response.readEntity(new GenericType<List<User1>>() {
+		});
+
+		for (User1 u : users) {
+			System.out.println(u);
+		}
+	}
+
+}
--- src/TestClient.java
+++ src/TestClient.java
@@ -10,34 +10,37 @@ import javax.ws.rs.core.Response;
 
 public class TestClient {
 
-	public static void main(String[] args) {
-		Client client = ClientBuilder.newClient();
-		Response response = client.target("http://localhost:8080/UserManagement/webservice/users")
-				.request().buildGet().invoke();
-		
-		User1 user2 = response.readEntity(User1.class);
-		System.out.println(user2);
-		response.close();
-		
-		User1 alfred = new User1();
-		alfred.setName("Alfred");
-		alfred.setSurname("Alfredsson");
-		
-		
-		Entity alfredity = Entity.entity(alfred, "application/XML");
-		
-		response = client.target("http://localhost:8080/UserManagement/webservice/users")
-				.request().buildPost(alfredity).invoke();
-		System.out.println(response.readEntity(User1.class).getId());
-		response.close();
-		
-		
-		response = client.target("http://localhost:8080/UserManagement/webservice/users")
-				.request().buildGet().invoke();
-		List<User1> users = response.readEntity(new GenericType<List<User1>>() {});
-		
-		for (User1 u : users) {
-			System.out.println(u);
-		}
+	public static void main(String[] args) {	
+		RunApp a = new RunApp();
+		a.appRunner();
+
+//		Client client = ClientBuilder.newClient();
+//		Response response = client.target("http://localhost:8080/UserManagement/webservice/users/")
+//				.request().buildGet().invoke();
+//		
+//		User1 user2 = response.readEntity(User1.class);
+//		System.out.println(user2);
+//		response.close();
+//		
+//		User1 alfred = new User1();
+//		alfred.setName("Alfred");
+//		alfred.setSurname("Alfredsson");
+//		
+//		
+//		Entity alfredity = Entity.entity(alfred, "application/XML");
+//		
+//		response = client.target("http://localhost:8080/UserManagement/webservice/users")
+//				.request().buildPost(alfredity).invoke();
+//		System.out.println(response.readEntity(User1.class).getId());
+//		response.close();
+//		
+//		
+//		response = client.target("http://localhost:8080/UserManagement/webservice/users")
+//				.request().buildGet().invoke();
+//		List<User1> users = response.readEntity(new GenericType<List<User1>>() {});
+//		
+//		for (User1 u : users) {
+//			System.out.println(u);
+//		}
 	}
 }
